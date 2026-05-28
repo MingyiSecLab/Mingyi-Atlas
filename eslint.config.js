8819344c82b2515388d08214501705a7e21991af
starrepository <starrepository@gmail.com>
2026-05-28 22:54:20 +0800

    init

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+/** @type {import("eslint").Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['**/*.ts?(x)'],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    ignores: ['scripts/**'],
+  },
+];
